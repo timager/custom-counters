3272c491ffa00fb8cca7792361e6c00e1b262de3
timager <timger98@gmail.com>
2021-04-03 19:35:10 +0300

    Разработана страница отображающая случайное число от 1 до 6 (игральный кубик) по центру экрана при загрузке

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import './App.css'
 
 function App () {
   return (
-    <div className="App">
-      123
+    <div className={'app'}>
+      <div className={'random-number'}>
+        {getRandomInDiapason(1, 6)}
+      </div>
     </div>
   )
+}
+
+function getRandomInDiapason (min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max + 1)
+  return Math.floor(Math.random() * (max - min)) + min
 }
 
 export default App
