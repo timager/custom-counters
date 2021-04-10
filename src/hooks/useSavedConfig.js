493cf248ac6c5439e2e2f42ec7678d323a687f4e
timager <timger98@gmail.com>
2021-04-10 19:44:01 +0300

    Добавил обработку ошибки парсинга JSON

--- src/hooks/useSavedConfig.js
+++ src/hooks/useSavedConfig.js
@@ -1,11 +1,17 @@
-export const DEFAULT_CONFIG = {
+const DEFAULT_CONFIG = {
   min: 1,
   max: 6,
 }
 
 export function useSavedConfig () {
-  let saved = JSON.parse(localStorage.getItem('config'))
-  let config = { ...DEFAULT_CONFIG, ...(saved ?? []) }
+  let config
+  try {
+    let saved = JSON.parse(localStorage.getItem('config'))
+    config = { ...DEFAULT_CONFIG, ...(saved ?? []) }
+  }
+  catch (e) {
+    config = DEFAULT_CONFIG
+  }
   const saveConfig = c => localStorage.setItem('config', JSON.stringify(c))
   return [config, saveConfig]
 }
