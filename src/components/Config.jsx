0a7ffe7e0b7a0f5b28fd6260cfcff29dc0cfc30c
timager <timger98@gmail.com>
2021-04-17 19:26:16 +0300

    Новые иконки и мобильная адаптивность

--- src/components/Config.jsx
+++ src/components/Config.jsx
@@ -3,15 +3,20 @@ import { useConfig } from '../hooks/useConfig'
 export function Config () {
   const [min, setMin] = useConfig('min')
   const [max, setMax] = useConfig('max')
+
+  function validateIntVal (value) {
+    return Math.round(Number(value.replaceAll(/[^\d]/g, '').substr(0, 3)))
+  }
+
   return (
     <div className={'config'}>
       <label>От
-        <input type='number' value={min}
-               onChange={e => setMin(Number(e.target.value))}/>
+        <input value={min}
+               onChange={e => setMin(validateIntVal(e.target.value))}/>
       </label>
       <label>До
-        <input type='number' value={max}
-               onChange={e => setMax(Number(e.target.value))}/>
+        <input value={max}
+               onChange={e => setMax(validateIntVal(e.target.value))}/>
       </label>
     </div>
   )
