26a633399449ffb2dc4301a21c2cd5dccf9ec81f
timager <timger98@gmail.com>
2021-04-17 17:52:54 +0300

    pwa is configured

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -11,4 +12,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+serviceWorkerRegistration.register();
 reportWebVitals();
